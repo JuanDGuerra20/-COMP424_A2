f569a0396df7c90a5df94e3b29c0426ec0e2a764
Juan David Guerra <juandavidguerra1@gmail.com>
2022-10-26 21:28:18 -0400

    fixed some logic issues, miight have to restart honestly

--- src/A2Q1_nQueens.java
+++ src/A2Q1_nQueens.java
@@ -1,11 +1,14 @@
 import java.util.Arrays;
 import java.util.Random;
+import java.util.PriorityQueue;
 
 public class A2Q1_nQueens {
     public static int n = 5;
     public int[][] boardState = new int[n][n];
     public int[][] constraintGraph = new int[n][n];
     public int[] filledColumns = new int[n];
+
+    public int
     public void addConstraints(int qx,int qy){
         constraintGraph[qx][qy] ++;
         for(int count = 1; count < n ; count ++){
@@ -50,73 +53,32 @@ public class A2Q1_nQueens {
     }
     public void remConstraints(int qx,int qy){
         // This will check if there are any other queens in the same row to the right
-        constraintGraph[qx][qy] --;
-        for(int count = 1; count < n ; count ++){
-            // checking the diagonal direction down and right
-            if(qx + count < n && qy + count < n){
-                constraintGraph[qx + count][qy + count] --;
-            }
-            // Checking diagonal direction up and right
-            if(qx + count < n && qy-count >= 0){
-                constraintGraph[qx][qy - count] --;
-            }
-            // Checking diagonal direction down and left
-            if(qx - count >= 0 && qy + count < n){
-                constraintGraph[qx-count][qy + count] --;
-            }
-            // Checking diagonal direction up and left
-            if(qx - count >= 0 && qy - count >= 0){
-                constraintGraph[qx-count][qy-count] --;
-            }
-            if(boardState[count][qy] == 0){
-                constraintGraph[count][qy] --;
-
-            }
-            if(qx - count >= 0 && boardState[qx-count][qy] ==0){
-                if(qx - count >= 0){
-                    constraintGraph[qx - count][qy] --;
-                }
-            }
-
-            //checking the column
-            if(boardState[qx][count] == 0){
-                constraintGraph[qx][count] --;
-
-            }
-
-            if(qy - count >= 0 && boardState[qx][qy - count] == 0){
-                constraintGraph[qx][qy - count] --;
-            }
-        }
+        constraintGraph = new int[n][n];
     }
 
     public void createBoard(){
         Random rand = new Random();
-        int x;
         int y;
-        boolean flag = true;
         // randomly assigning the queens to different positions in the board
         // a 1 at a position means there is a queen present
+        // will be assigned one at each column
         for(int i = 0; i < n; i ++){
-            while(flag){
-                x = rand.nextInt(n);
-                y = rand.nextInt(n);
-                if(boardState[x][y] == 0) {
-                    boardState[x][y] = 1;
-                    flag = false;
-                }
+            y = rand.nextInt(n);
+            if(boardState[i][y] == 0) {
+                boardState[i][y] = 1;
             }
-            flag = true;
+
         }
     }
     public void updateConstraintGraph(boolean moveQueen){
-
+        int[][] newGraph = new int[n][n];
         if(moveQueen){
             for (int y = 0; y < n; y ++) {
                 if (filledColumns[y] == 0) {
                     boardState[1][y] = 1;
                     addConstraints(1, y);
                     moveQueen = false;
+                    break;
                 }
             }
         }
@@ -135,7 +97,7 @@ public class A2Q1_nQueens {
             }
         }
     }
-    public int getBadQueens(boolean move){
+    public int getBadQueens(int [][] boardState, int [][] constraintGraph){
         int numQFound = 0;
         int x = 0;
         int y = 0;
@@ -147,11 +109,24 @@ public class A2Q1_nQueens {
             else {
                 y++;
             }
-            if(constraintGraph[x][y] == 1 && boardState[x][y] == 1){
+            if(constraintGraph[x][y] > 1 && boardState[x][y] == 1){
                 numQFound ++;
             }
         }
         return numQFound;
+    }
+
+    public void findBestNeighbour(){
+        // This will update the boardState with the best neighbour boardState
+        int[][] bestBoard = boardState;
+        int bestHeuristic = getBadQueens(boardState, constraintGraph);
+
+        int [][] neighbourBoard = new int[n][n];
+        for(int i = 0; i < n ; i ++){
+            for(int j = 0; j < n; j ++){
+                neighbourBoard[i][j] = boardState[i][j];
+            }
+        }
     }
     public int moveBadQueen(){
         /*
@@ -177,10 +152,12 @@ public class A2Q1_nQueens {
         while(chess.moveBadQueen() == 1){
             for(int i = 0 ; i < n; i ++){
                 System.out.println(Arrays.toString(chess.boardState[i]));
+
             }
+            System.out.println("+===========================");
 
         }
-        System.out.println("+===========================");
+
         for(int i = 0 ; i < n; i ++){
             System.out.println(Arrays.toString(chess.boardState[i]));
         }
